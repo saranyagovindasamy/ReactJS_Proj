2e9f913833a141b413367ade483d17377ca516a5
Saranya Govindasamy <saranyaarung@gmail.com>
2023-08-19 13:43:00 +0700

    Portfolio project finished

---
+++ Saranya_Portfolio/components/AboutSection.tsx
@@ -0,0 +1,104 @@
+"use client"
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "React" },
+  {skill:"NextJs"},
+  {skill:"SASS"},
+  { skill: "Bootstrap" },
+  {skill:"TailwindCss"},
+  {skill:"Antd"},
+  { skill: "Node.js" },
+  {skill:"ExpressJs"},
+  { skill: "GitLab" },
+  { skill: "PostgreSql" },
+  { skill: "MongoDb" },
+  {skill:"MySql"},
+  {skill:"Jest"},
+  {skill:"cypress"},
+  {skill:"Jira"},
+  {skill:"Agile Methodology"},
+  {skill:"Docker"},
+  {skill: "AWS"},
+  {skill:"Digital Ocean"}
+]
+
+const AboutSection = () => {
+
+
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Saranya and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> software engineer
+              based in Ho Chi Minh, Vietnam.
+            </p>
+            <br />
+            <p>
+              I am a Masters Degree holder in the field of Computer Engineering and have been working in the
+              field ever since.
+            </p>
+            <br />
+            <p>
+              I have a wide range of hobbies and passions that keep me busy.
+              From reading books, yoga, learning advanced concepts of coding. 
+              I am always seeking new experiences and love to keep myself
+              engaged and learning new things.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <button
+            className="text-neutral-100 font-semibold px-6 py-3 mt-5 bg-teal-600 rounded shadow hover:bg-teal-700">
+        
+            <a href='/Saranya_Govindasamy_2023_FE.pdf' download>Download Resume</a>
+          </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
